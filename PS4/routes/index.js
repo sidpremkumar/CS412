13d7c93769349bb1a2ff38224d03572d7baf5fe0
Sid Premkumar <sid.premkumar@gmail.com>
2020-10-26 17:11:21 -0400

    Add PS4

---
+++ PS4/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const https = require('https')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // Get all the countries\
+  const url = 'https://api.covid19api.com/countries'
+  https.get(url, (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        const dataFormatted = JSON.parse(data);
+        let countries = []
+        for (country in dataFormatted){
+          countries.push({'Country': dataFormatted[country].Country, 'Slug': dataFormatted[country].Slug})
+        }
+        // Display all countries as an option to the user 
+        res.render('index', { countries: countries });
+    });
+
+    }).on("error", (err) => {
+        res.render('errorMessage');
+});
+
+  
+});
+
+module.exports = router;
---
+++ PS4/routes/ps4.js
@@ -0,0 +1,41 @@
+var express = require('express');
+const https = require('https')
+var router = express.Router();
+
+// API used: https://documenter.getpostman.com/view/10808728/SzS8rjbc
+
+router.post('/', function(req, res, next) {
+    // Get the name of the coutnry we're looking at
+    console.log(req.body)
+    const countryName = req.body.country;
+    
+
+    // Make our API call
+    // https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
+    const url = 'https://api.covid19api.com/country/' + countryName + '/status/confirmed'
+    https.get(url, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            const dataFormatted = JSON.parse(data);
+            const dataToday = dataFormatted[dataFormatted.length-1]
+            res.render('ps4Response', { country: countryName, cases: dataToday.Cases});
+
+        });
+
+        }).on("error", (err) => {
+            res.render('errorMessage');
+    });
+    
+    
+      
+});
+
+module.exports = router;
+
